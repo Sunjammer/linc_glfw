720cbf9a4abede3d277294b60770019e64a16c11
George Corney <haxiomic@gmail.com>
2019-05-13 21:19:15 +0100

    Remove trace for error message

--- glfw/GLFW.hx
+++ glfw/GLFW.hx
@@ -31,7 +31,6 @@ class GLFWErrorHandler {
     static var cb:GLFWerrorfun;
 
     static public function nativeCallack(error:Int, message:cpp.ConstCharStar){
-        trace(error+": "+message);
         if(cb != null) cb(error, message);
     }
 
